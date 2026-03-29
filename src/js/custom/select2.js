7e517104a0146830c1e9365c4872975b73c6c3c2
mizza-cz <mizza.cz666@gmail.com>
2026-03-29 16:40:51 +0200

    fix js

--- src/js/custom/select2.js
+++ src/js/custom/select2.js
@@ -1,10 +1,12 @@
 $(".js-select").select2({
   width: "100%",
   minimumResultsForSearch: -1,
+  dropdownParent: $(".js-select").parent(),
 });
 
 $(function () {
   $(".js-select").change(function () {
+    dropdownParent: $(".js-select").parent();
     $(".subPageFilter").submit();
   });
 });
