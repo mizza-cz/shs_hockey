3de8362e50f9822b0546760949289861befed3c1
mizza-cz <mizza.cz666@gmail.com>
2026-09-14 11:30:47 +0200

    add url modal

--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -1,12 +1,29 @@
 document.addEventListener("click", function (e) {
-  const a = e.target.closest('a[data-bs-toggle="modal"]');
-  if (a) e.preventDefault();
+  const link = e.target.closest('a[data-bs-toggle="modal"][data-product]');
+
+  if (!link) return;
+
+  e.preventDefault();
+
+  const product = link.dataset.product;
+  const url = new URL(window.location.href);
+
+  url.searchParams.set("product", product);
+
+  history.pushState({ product }, "", url);
 });
 
 document.querySelectorAll(".modal").forEach((modal) => {
   modal.addEventListener("hidden.bs.modal", function () {
+    const url = new URL(window.location.href);
+
+    url.searchParams.delete("product");
+
+    history.replaceState({}, "", url);
+
     modal.querySelectorAll("iframe").forEach((iframe) => {
       const src = iframe.src;
+
       iframe.src = "";
       iframe.src = src;
     });
